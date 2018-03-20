60cd69afc3caebc22cad2f42c3841834a5b453bb
kuryukin.a <nuveum@gmail.com>
2018-03-20 10:18:02 +0300

    Fix #15697: Extra markup removed

--- src/app/modules/roles/components/Controls.js
+++ src/app/modules/roles/components/Controls.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Input } from 'reactstrap';
-import { DatePicker } from 'react-widgets'
 
 import _ from 'lodash';
 
@@ -42,7 +41,6 @@ class RolesControls extends React.PureComponent {
                 <div className={styles.buttonGroup}>
                     <Icon icon="addIcon" onClick={this.onAdd} />
                     <Icon icon="deleteIcon" onClick={this.props.onRemove} />
-                    <DatePicker/>
                 </div>
                 <Input placeholder={ls('SERCH_PLACEHOLDER', 'Поиск')}
                        className={styles.rolesSearch}
