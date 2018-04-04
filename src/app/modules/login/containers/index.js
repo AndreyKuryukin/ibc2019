0be80adbfaed6835ce1bed56073f89b13a0c3782
kuryukin.a <nuveum@gmail.com>
2018-04-04 13:13:27 +0300

    Fix #15090: Form validation added

--- src/app/modules/login/containers/index.js
+++ src/app/modules/login/containers/index.js
@@ -10,6 +10,7 @@ import rest, { signIn } from '../../../rest/index';
 import { ERRORS } from "../../../costants/errors";
 import ls from "i18n";
 import { LOGIN_REQUEST, SIGN_IN_URL } from "../../../costants/login";
+import { validateForm } from "../../../util/validation";
 
 
 class Login extends React.PureComponent {
@@ -17,6 +18,15 @@ class Login extends React.PureComponent {
     static contextTypes = {
         navBar: PropTypes.object.isRequired,
         notifications: PropTypes.object.isRequired,
+    };
+
+    validationConfig = {
+        login: {
+            required: true
+        },
+        password: {
+            required: true
+        }
     };
 
     componentDidMount() {
@@ -29,24 +39,32 @@ class Login extends React.PureComponent {
     }
 
     onSubmit = (login, password) => {
-        this.setState({ loading: true });
-        rest.post(SIGN_IN_URL, {
+        const loginForm = { login, password };
+        const errors = validateForm(loginForm, this.validationConfig);
+        if (!_.isEmpty(errors)) {
+            this.setState({ errors });
+        } else {
+            this.setState({ loading: true, errors: {} });
+            rest.post(SIGN_IN_URL, {
                 [LOGIN_REQUEST.LOGIN]: login,
-                [LOGIN_REQUEST.PASSWORD]: password,
+                [LOGIN_REQUEST.PASSWORD]: password
             })
-            .then((userName) => {this.setState({ loading: false });
-                this.props.onLoginSuccess(userName);
-                this.context.notifications.close('login-failed');
-                this.props.history.push('/roles');
-            }).catch((error) => {
-            this.context.notifications.notify({
-                title: ls('LOGIN_ERROR_FIELD', 'Неверные учётные данные:'),
-                message: ls('LOGIN_ERROR_FIELD', 'Логин и пароль не совпадают'),
-                type: 'CRITICAL',
-                code: 'login-failed'
-            });
-            this.setState({ errors: _.get(error, `data.${ERRORS}`), loginFailed: true });
-        });
+                .then((userName) => {
+                    this.setState({ loading: false });
+                    this.props.onLoginSuccess(userName);
+                    this.context.notifications.close('login-failed');
+                    this.props.history.push('/roles');
+                })
+                .catch((error) => {
+                    this.context.notifications.notify({
+                        title: ls('LOGIN_ERROR_FIELD', 'Неверные учётные данные:'),
+                        message: ls('LOGIN_ERROR_FIELD', 'Логин и пароль не совпадают'),
+                        type: 'CRITICAL',
+                        code: 'login-failed'
+                    });
+                    this.setState({ errors: _.get(error, `data.${ERRORS}`) });
+                });
+        }
     };
 
     render() {
@@ -54,7 +72,6 @@ class Login extends React.PureComponent {
             onSubmit={this.onSubmit}
             loading={this.state.loading}
             errors={this.state.errors}
-            loginFailed={this.state.loginFailed}
         />);
     }
 }
