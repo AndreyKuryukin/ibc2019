fbae3b470971cb5fd683e4323eed075bb59e2a59
kuryukin.a <nuveum@gmail.com>
2018-04-24 14:57:48 +0300

    Fix #16211: basic structure

---
+++ src/app/modules/crashes/components/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Input } from 'reactstrap';
+import classnames from 'classnames';
+
+class CrashesCmp extends React.PureComponent {
+    render () {
+
+    }
+}
+
+export default CrashesCmp;
