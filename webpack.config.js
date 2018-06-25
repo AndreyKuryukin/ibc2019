0afec8dc8d0aa1cb64abb8e0145ec87a83c27bd4
kuryukin.a <nuveum@gmail.com>
2018-06-25 12:25:06 +0300

    Build failure fix

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const PROXY_HOST = process.env.PROXY_HOST;
 const PROXY_PORT = process.env.PROXY_PORT;
 
 if (prodMode) {
-    plugins.push(new MinifyPlugin());
+    // plugins.push(new MinifyPlugin());
 }
 
 if (devMode) {
