12768f2b8666067a7eb5ea922e4e39a0e6b3d21f
nikita.stankevich <nikita.stankevich@logic-way.com>
2018-08-15 17:14:20 +0300

    Fix #17926: Columns applied

--- src/app/modules/alarms/components/AlarmsTable.js
+++ src/app/modules/alarms/components/AlarmsTable.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import memoize from 'memoizejs';
 import moment from 'moment';
-import ls from 'i18n';
+import ls, { createLocalizer } from 'i18n';
 import _ from 'lodash';
 import search from '../../../util/search';
 import Table from '../../../components/Table';
 import { convertUTC0ToLocal } from '../../../util/date';
 import { naturalSort } from '../../../util/sort';
 import { DefaultCell, LinkCell, IconCell } from '../../../components/Table/Cells';
-import { ALARMS_TYPES } from '../constants';
+import { ALARMS_TYPES, CLIENTS_INCIDENTS_ALARMS } from '../constants';
 
 const iconCellStyle = {
     display: 'flex',
@@ -18,65 +18,110 @@ const iconCellStyle = {
 };
 
 class AlarmsTable extends React.PureComponent {
+    static contextTypes = {
+        match: PropTypes.object.isRequired,
+    };
+
     static propTypes = {
         type: PropTypes.oneOf(ALARMS_TYPES).isRequired,
         data: PropTypes.array,
+        total: PropTypes.number,
         searchText: PropTypes.string,
         preloader: PropTypes.bool,
     };
 
     static defaultProps = {
         data: [],
+        total: 0,
         searchText: '',
         preloader: false,
     };
 
-    static getColumns = memoize(() => ([
-        {
-            getTitle: () => ls('ALARMS_ID_COLUMN', 'ID'),
-            name: 'id',
-            resizable: true,
-            searchable: true,
-            sortable: true,
-        }, {
-            getTitle: () => ls('ALARMS_EXTERNAL_ID_COLUMN', 'ID во внешней системе'),
-            name: 'external_id',
-            resizable: true,
-            searchable: true,
-            sortable: true,
-            searchable: true,
-            width: 150,
-        }, {
-            getTitle: () => ls('ALARMS_POLICY_NAME_COLUMN', 'Имя политики'),
-            name: 'policy_name',
-            resizable: true,
-            searchable: true,
-            sortable: true,
-        }, {
-            getTitle: () => ls('ALARMS_NOTIFICATION_STATUS_COLUMN', 'Статус отправки во внешнюю систему'),
-            name: 'notification_status',
-            sortable: true,
-            width: 250,
-        }, {
-            getTitle: () => ls('ALARMS_RAISE_TIME_COLUMN', 'Время возникновения'),
-            name: 'raise_time',
-            searchable: true,
-            sortable: true,
-            width: 150,
-        }, {
-            getTitle: () => ls('ALARMS_DURATION_COLUMN', 'Длительность'),
-            name: 'duration',
-            searchable: true,
-            sortable: true,
-            width: 100,
-        }, {
-            getTitle: () => ls('ALARMS_OBJECT_COLUMN', 'Объект'),
-            name: 'object',
-            resizable: true,
-            searchable: true,
-            sortable: true,
-        }
-    ]));
+    static getColumns = memoize((type = CLIENTS_INCIDENTS_ALARMS) => {
+        const commonColumns = [
+            {
+                getTitle: createLocalizer('ALARMS_ID_COLUMN', 'ID'),
+                name: 'id',
+                resizable: true,
+                searchable: true,
+                sortable: true,
+            }, {
+                getTitle: createLocalizer('ALARMS_EXTERNAL_ID_COLUMN', 'ID во внешней системе'),
+                name: 'external_id',
+                resizable: true,
+                searchable: true,
+                sortable: true,
+                searchable: true,
+                width: 150,
+            }, {
+                getTitle: createLocalizer('ALARMS_STATUS_COLUMN', 'Статус'),
+                name: 'status',
+                sortable: true,
+                resizable: true,
+                width: 100,
+            }, {
+                getTitle: createLocalizer('ALARMS_POLICY_NAME_COLUMN', 'Имя политики'),
+                name: 'policy_name',
+                resizable: true,
+                searchable: true,
+                sortable: true,
+            }, {
+                getTitle: createLocalizer('ALARMS_NOTIFICATION_STATUS_COLUMN', 'Статус отправки во внешнюю систему'),
+                name: 'notification_status',
+                sortable: true,
+                width: 250,
+            }, {
+                getTitle: createLocalizer('ALARMS_RAISE_TIME_COLUMN', 'Дата и время возникновения'),
+                name: 'raise_time',
+                searchable: true,
+                sortable: true,
+                width: 150,
+            },  {
+                getTitle: createLocalizer('ALARMS_CEASE_TIME_COLUMN', 'Дата и время закрытия'),
+                name: 'cease_time',
+                resizable: true,
+                searchable: true,
+                sortable: true,
+                width: 150,
+            }, {
+                getTitle: createLocalizer('ALARMS_DURATION_COLUMN', 'Длительность'),
+                name: 'duration',
+                searchable: true,
+                sortable: true,
+                width: 100,
+            }
+        ];
+
+        const columnsByType = type === CLIENTS_INCIDENTS_ALARMS
+                ? [{
+                    getTitle: createLocalizer('ALARMS_MAC_COLUMN', 'MAC'),
+                    name: 'mac',
+                    resizable: true,
+                    searchable: true,
+                    sortable: true,
+                }, {
+                    getTitle: createLocalizer('ALARMS_SAN_COLUMN', 'Service account number (SAN)'),
+                    name: 'san',
+                    resizable: true,
+                    searchable: true,
+                    sortable: true,
+                } , {
+                    getTitle: createLocalizer('ALARMS_PERSONAL_ACCOUNT_COLUMN', 'Лицевой счёт'),
+                    name: 'personal_account',
+                    resizable: true,
+                    searchable: true,
+                    sortable: true,
+                }]
+                : [{
+                    getTitle: createLocalizer('ALARMS_OBJECT_COLUMN', 'Объект'),
+                    name: 'object',
+                    resizable: true,
+                    searchable: true,
+                    sortable: true,
+                }];
+
+        return commonColumns.concat(columnsByType);
+    });
 
     headerRowRender = (column, sort) => (
         <DefaultCell
@@ -134,8 +179,13 @@ class AlarmsTable extends React.PureComponent {
         policy_name: node.policy_name,
         notification_status: node.notification_status || '',
         raise_time: convertUTC0ToLocal(node.raise_time).format('HH:mm DD.MM.YYYY'),
+        cease_time: node.cease_time ? convertUTC0ToLocal(node.cease_time).format('HH:mm DD.MM.YYYY') : '',
         duration: this.getReadableDuration(node.duration),
         object: node.object || '',
+        personal_account: node.personal_account || '',
+        san: node.san || '',
+        mac: node.mac || '',
+        status: node.status || '',
         timestamp: convertUTC0ToLocal(node.raise_time).valueOf(),
     })));
 
@@ -148,8 +198,8 @@ class AlarmsTable extends React.PureComponent {
     filter = (data, searchableColumns, searchText) => data.filter(node => searchableColumns.find(column => search(node[column.name], searchText)));
 
     render() {
-        const { data, searchText, preloader } = this.props;
-        const columns = AlarmsTable.getColumns();
+        const { data, searchText, preloader, total } = this.props;
+        const columns = AlarmsTable.getColumns(this.context.match.params.type);
         const mappedData = this.mapData(data);
         const filteredData = searchText ? this.filter(mappedData, columns.filter(col => col.searchable), searchText) : mappedData;
         console.log('Table render');
@@ -163,6 +213,7 @@ class AlarmsTable extends React.PureComponent {
                 bodyRowRender={this.bodyRowRender}
                 preloader={preloader}
                 showStatistics
+                total={total}
             />
         );
     }
